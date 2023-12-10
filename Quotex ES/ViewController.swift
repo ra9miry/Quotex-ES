250a04dd89899666fdeab2e60d1184c2f0f84d42
Radmiry <telmanradmir@gmail.com>
2023-12-10 14:38:37 +0600

    Initial Commit

---
+++ Quotex ES/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quotex ES
+//
+//  Created by Радмир Тельман on 10.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
